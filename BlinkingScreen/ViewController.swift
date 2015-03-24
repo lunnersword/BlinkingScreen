c29a55147f905e7177323a28db84e0bd89a88f4f
lunner <lunner@lunners-Mac-mini.local>
2015-03-24 20:49:54 +0800

    Initial Commit

---
+++ BlinkingScreen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlinkingScreen
+//
+//  Created by lunner on 3/24/15.
+//  Copyright (c) 2015 lunner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
